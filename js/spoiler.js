11ec115b75ad83354f31518c6c359b4818a628b1
MucahitSaratar <trregen222@gmail.com>
2021-04-15 17:47:00 +0300

    javascript; allah belanı version
    Signed-off-by: MucahitSaratar <trregen222@gmail.com>

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,4 +1,14 @@
 function showme() {
+    var body = document.body;
+    body.classList.add("lanetclass");
     document.getElementById("spoileronleyici").style.display = "none";
     document.getElementById("spoiler").style.display = "inline";
 }
+
+function degistir(rengim) {
+    document.body.style.backgroundColor = rengim
+}
+
+//window.onmousedown = function(olay) {
+//    var body = document.body;
+//    body.classList.add("lanetclass"); }
